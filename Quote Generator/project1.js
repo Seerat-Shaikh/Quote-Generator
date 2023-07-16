d0bcb12142ea3b4eed1e71fcd245724c9ce86a33
Seerat Shaikh <129963092+Seerat-Shaikh@users.noreply.github.com>
2023-07-16 13:17:46 +0500

    Js File

--- Quote Generator/project1.js
+++ Quote Generator/project1.js
@@ -7,19 +7,24 @@ let person = document.querySelector('.person');
 const quotes = [{
     quote:`"Fill your paper with the breathings of your heart."`,
     person: `William Wordsworth`
-}, {
+  },
+  {
     quote: `"The art of writing is the art of discovering what you believe."`,
     person: ` Gustav Flaubert`
-},  {
+  },  
+  {
     quote:`"Fill your paper with the breathings of your heart."`,
     person: `William Wordsworth`
-},  {
+  },  
+  {
     quote:`"It’s the possibility of having a dream come true that makes life interesting."`,
     person: ` Paulo Coelho`
-},  {
+  }, 
+  {
     quote:`"Creativity is a combination of discipline and childlike spirit."`,
     person: `  Robert Greene`
-}, ];
+  }, 
+  ];
 
 btn.addEventListener('click', function(){
     
